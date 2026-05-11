68574ca30aace957ea158969334f87b249e063b6
Seu Nome <seuemail@gmail.com>
2026-05-11 12:30:38 -0300

    corrige pagina videos

--- app/videos/page.tsx
+++ app/videos/page.tsx
@@ -7,7 +7,6 @@ export default function Videos() {
         <div className="max-w-7xl mx-auto">
 
           <div className="mb-16">
-
             <p className="text-green-700 text-lg font-semibold mb-3">
               Conteúdo Técnico
             </p>
@@ -23,22 +22,33 @@ export default function Videos() {
               Acompanhe vídeos, orientações técnicas, resultados de campo
               e conteúdos voltados à saúde e produtividade animal.
             </p>
-
           </div>
 
           <div className="grid md:grid-cols-3 gap-10">
 
             <div className="bg-[#f8f8f8] rounded-[30px] overflow-hidden shadow-lg">
 
-              <div className="relative w-full overflow-hidden rounded-t-[30px] bg-black flex justify-center">
+              <video
+                controls
+                playsInline
+                className="w-full aspect-[9/16] object-cover bg-black"
+              >
+                <source
+                  src="https://fqpswxyjssiaqpvmgngg.supabase.co/storage/v1/object/public/Servicos/SaveClip.App_AQNFSKD3PFMBik8W23yhsCmMYQbJuA83AwUngQFF7fEKnnxtHXcqTlzDIy0RgNYlE8sjJyUbPomRMl_KKFD8080y.mp4"
+                  type="video/mp4"
+                />
+              </video>
 
-  <iframe
-    src="https://www.instagram.com/reel/DTObmf5jl7u/embed"
-    className="w-full max-w-[380px] h-[680px]"
-    allowTransparency={true}
-  ></iframe>
+              <div className="p-8">
+                <h2 className="text-2xl text-green-900 font-bold">
+                  3 erros mais comuns na vacinação
+                </h2>
 
-</div>
+                <p className="mt-4 text-gray-700 leading-8">
+                  Orientações práticas para melhorar o manejo sanitário e evitar
+                  falhas durante a vacinação.
+                </p>
+              </div>
 
             </div>
 
