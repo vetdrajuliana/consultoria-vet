ddab1819980f5ead2cb4f7bb786f7900af32712c
Seu Nome <seuemail@gmail.com>
2026-06-15 12:18:32 -0300

    Redireciona dominio antigo para appecuaria

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "julianamoraesvet.com.br",
+          },
+        ],
+        destination: "https://appecuaria.com.br/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.julianamoraesvet.com.br",
+          },
+        ],
+        destination: "https://appecuaria.com.br/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
